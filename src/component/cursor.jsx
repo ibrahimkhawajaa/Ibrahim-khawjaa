d6691e5a81dc27ccf04d5bbabe3391f0261c13f0
ibrahim <ibrahimmkhawajaa@gmail.com>
2023-10-27 16:47:20 +0500

    yes-last

--- src/component/cursor.jsx
+++ src/component/cursor.jsx
@@ -1,31 +1,9 @@
 
-import { CircleCursor } from 'react-cursors'
+
 export default function cursor() {
   return (
     <div>
-      <CircleCursor 
-        safeBoundaryX="1000"
-        safeBoundaryY="700"
-        initial={{
-                dotSize:"2rem",
-                circleSize:"5rem",
-                borderStyle:"dotted",
-                color:"white",
-                borderWidth:"3px",
-                circleDay:"0.1s",
-                dotDelay:"0.05s"
-        }}
-
-        hover={{
-                dotSize:"0rem",
-                circleSize:"10rem",
-                borderStyle:"dotted",
-                color:"white",
-                borderWidth:"3px",
-                circleDay:"0.1s",
-                dotDelay:"0.05s"
-        }}
-/>
+      
     </div>
   )
 }
