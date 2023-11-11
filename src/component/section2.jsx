397fb0d48377db3ce696cd48bdfe2eb460b0f64b
ibrahim <ibrahimmkhawajaa@gmail.com>
2023-11-11 22:48:26 +0500

    new-web-completed

--- src/component/section2.jsx
+++ src/component/section2.jsx
@@ -3,13 +3,13 @@ import Img1 from "./Img/wa.svg";
 
 export default function section2() {
   return (
-    <div className=" flex justify-around items-center flex-wrap w-auto h-auto h-screen   ">
+    <div className="border-t border-black border-b flex justify-around items-center flex-wrap w-auto h-auto h-screen   ">
       <div className="  ">
         <Image src={Img1} />
       </div>
       <div className="text-center  w-full sm:w-1/2  ">
-        <h1 className=" text-[#90EE20] text-6xl  text-Grotesk sm:7xl ">Who am i?</h1>
-        <p className="text-center text-Neue text-2xl  sm:text-5xl ">
+        <h1 className=" text-[#90EE20] text-6xl  text-Grotesk sm:text-8xl ">Who am i?</h1>
+        <p className="text-center text-Neue text-2xl   sm:text-5xl ">
           {" "}
           I'm <b  className="underline">Ibrahim</b>, a <b>React Developer</b> with over{" "}
           <b className="underline"> 2+ years</b> of professional experience in the world of web
