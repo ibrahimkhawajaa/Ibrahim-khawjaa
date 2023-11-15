ec1b4d3a1cb2e27ced0026631cb3b8b10d38a2f0
ibrahim <ibrahimmkhawajaa@gmail.com>
2023-11-15 14:55:23 +0500

    new-1

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -5,7 +5,7 @@ export default function navbar() {
   return (
     <nav className=" relative   p-10  pt-10 h-[20vh] sm:pt-20 flex justify-between text-center">
       <div>
-        <h1 className="text-logo text-2xl sm:text-5xl xl:relative pl-0   ">
+        <h1 className="text-logo text-2xl relative right-[30px] sm:text-5xl  xl:relative    ">
        Ibrahim
      
         </h1>
